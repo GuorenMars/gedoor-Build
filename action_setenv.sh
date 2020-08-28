00ad8739d05442ea5aa56b0969a5bdb8ad0f3f72
GuorenMars <69036516+GuorenMars@users.noreply.github.com>
2020-08-28 08:24:02 +0800

    Update action_setenv.sh

--- action_setenv.sh
+++ action_setenv.sh
@@ -2,15 +2,15 @@
 
 function set_env() { echo ::set-env name=$1::$2; }
 
-APP_NAME='$APP_NAME'
-APP_GIT_URL='https://github.com/gedoor/MyBookshelf.git'
+APP_NAME="legado"
+APP_GIT_URL="https://github.com/gedoor/legado.git"
 if [ $SECRETS_ENABLE = 'true' ] && [ -n "$SECRETS_APP_NAME" ] && [ -n "$SECRETS_GIT_URL" ]; then
     APP_NAME=$SECRETS_APP_NAME
     APP_GIT_URL=$SECRETS_GIT_URL
 fi
 APP_WORKSPACE="/opt/$APP_NAME"
 APP_LATEST_TAG='latest'
-APP_UPLOAD_NAME="$APP_UPLOAD_NAME-$APP_LATEST_TAG"
+APP_UPLOAD_NAME="$APP_NAME-$APP_LATEST_TAG"
 APP_UPLOAD="$APP_WORKSPACE/app/build/outputs/apk/app/release"
 
 set_env APP_NAME        $APP_NAME
